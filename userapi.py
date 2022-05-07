9e6e81f3b3763cce0cd1ba03242b99880bcb67bc
Joshua Eng <engch28@gmail.com>
2022-05-08 01:49:57 +0800

    updated post request function

--- userapi.py
+++ userapi.py
@@ -1,7 +1,10 @@
+from datetime import datetime
+from email.policy import default
 from flask import Flask, jsonify
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_pymongo import PyMongo
-from bson.json_util import loads, dumps, LEGACY_JSON_OPTIONS
+from bson.json_util import loads, dumps
+import json
 
 
 app = Flask(__name__)
@@ -9,29 +12,40 @@ api = Api(app)
 mongodb_client = PyMongo(app, uri="mongodb://localhost:27017/user_db")
 db = mongodb_client.db
 
-{
-"id": "xxx", # user ID
-"name": "test", # user name
-"dob": "", # date of birth
-"address": "", # user address
-"description": "", # user description
-"createdAt": "" # user created date
-}
-
 # db.user_db.insert({_id: NumberInt('1'), name: "josh", dob: "28-10-1990", address: "choa chu kang", description: "software engineer", createdAt: "06-05-2022 12:22:00"})
+# db.user_db.insert({_id: NumberInt('2'), name: "bosh", dob: "29-10-1990", address: "jurong east", description: "software engineer", createdAt: "07-05-2022 23:45:00"})
 # db.user_db.deleteOne({_id : 1})
+
+# set the arguments to be taken in during POST request
+parser = reqparse.RequestParser()
+parser.add_argument('name', type=str, help='Name of the user cannot be blank!', required=True)
+parser.add_argument('dob', type=lambda x: datetime.strptime(x,"%d/%m/%Y").date().strftime("%d/%m/%Y"), help='Date of Birth of the User must be in DD/MM/YYYY format, e.g 30/01/2022', required=True)
+parser.add_argument('address', type=str, help='Address of the user cannot be blank!', required=True)
+parser.add_argument('description', type=str, help='Description of the User')
+
 
 class UserAPI(Resource):
     def get(self):
-        userlist = []
+        # find all records from mongo and return them as json
         users = db.user_db.find()
-        # for user in users:
-        #     print(type(user))
-        #     userlist.append(user)
         return loads(dumps(users))
 
     def post(self):
-        return {'hello': 'world'}
+        # initialize the variable
+        last_user_id = None
+        args = parser.parse_args()
+        # retrieve the last record in the db
+        last_user = loads(dumps(db.user_db.find().limit(1).sort([( '$natural', -1 )] )))
+        # set the ID to 0 if no records or the ID of the last record if exist
+        if len(last_user) == 0:
+            last_user_id = 0
+        else:
+            last_user_id = last_user[0]['_id']
+        # insert the record into mongo with all the required values
+        db.user_db.insert_one({"_id": int(last_user_id+1), "name": args["name"], "dob": args["dob"], "address": args["address"], "description": args["description"], "createdAt": datetime.now().strftime("%d/%m/%Y %H:%M:%S")})
+        # retrive the lastest record and return as json
+        last_user = db.user_db.find().limit(1).sort([( '$natural', -1 )] )
+        return loads(dumps(last_user))
 
     def put(self):
         return {'hello': 'world'}
